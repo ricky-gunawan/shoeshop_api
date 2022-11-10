529f50b27d21a6029ce00ac8106390030280079d
Ricky Indra Gunawan <rickyindra006@gmail.com>
2022-11-10 19:34:28 +0700

    fix productdisplay controller

--- src/controller/getProductsDisplay.ts
+++ src/controller/getProductsDisplay.ts
@@ -3,7 +3,7 @@ import asyncHandler from "express-async-handler";
 import Product from "../models/productModel";
 
 const getProductsDisplay = asyncHandler(async (_, res: Response) => {
-  const products = await Product.aggregate([{ $sample: { size: 20 } }]);
+  const products = await Product.aggregate([{ $sample: { size: 10 } }]);
   res.json(products);
 });
 
