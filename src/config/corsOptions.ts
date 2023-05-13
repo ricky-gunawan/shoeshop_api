62aae00a6fd66c8d5d6fa8da2e698d9fa2e75429
ricky-gunawan <70275904+ricky-gunawan@users.noreply.github.com>
2023-05-13 09:51:38 +0700

    fix corsOptions.ts

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -2,7 +2,7 @@ import allowedOrigins from "./allowedOrigins";
 
 const corsOptions = {
   origin: (origin: any, callback: any) => {
-    if (allowedOrigins.indexOf(origin) !== 1) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(null, new Error("Not allowed by CORS"));
